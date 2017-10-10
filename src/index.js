1e15c5921ff915f44480711d9fe0306c219c1301
Krzysztof Glinka <krzysztof.m.glinka@gmail.com>
2017-10-10 23:42:38 +0200

    react, youtube API

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyAuWsoCBTGGEmSVEy869Hv1yo2fVAQVKqI';
+
+const App = () => {
+  return <div>"Hi"</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
